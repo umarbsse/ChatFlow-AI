5b20b23aea18608bf3480b0800ae1e959128d1fc
Umer Shahzad <umarbsse@gmail.com>
2026-05-07 23:36:43 +0500

    REact Acc Pages created successfully

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,26 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import Register from "./pages/account/Register";
+import Login from "./pages/account/Login";
+import ForgotPassword from "./pages/account/ForgotPassword";
+import Home from "./pages/home/Home";
 
 function App() {
-  return <Register />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+
+        <Route path="*" element={<Navigate to="/home" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
